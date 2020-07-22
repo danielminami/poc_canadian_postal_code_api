ac0dea29264e009793d49e1bb28f2c7be53ce953
Daniel <danielminami@hotmail.com>
2020-07-22 11:33:50 -0400

    Init Repo

---
+++ src/postalcodevalidator/PostalCodeValidator.java
@@ -0,0 +1,20 @@
+/*
+Daniel Minami <minamid@sheridancollege.ca>
+----------------------------------------------------------------------
+ */
+package postalcodevalidator;
+
+/**
+ *
+ * @author Daniel Minami <minamid@sheridancollege.ca>
+ */
+public class PostalCodeValidator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
